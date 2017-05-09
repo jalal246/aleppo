9552dbc8738421c6b6bc49a50d55c6b615f3f3d4
Jalal Maskoun <Jalal Maskoun>
2017-05-09 09:10:04 +0200

    remove parameters check

--- generators/numbers/numbers.js
+++ generators/numbers/numbers.js
@@ -1,23 +1,15 @@
 import {
   isNum,
   isStr,
-} from '../is';
+} from '..../is';
 
 // credit to: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range\
 const getRandom = (min, max, type = 'int') => {
-  if (isNum(min) && isNum(max) && isStr(type)) {
-    const getArbitrary = () => (Math.random() * (max - min)) + min;
-    const getInt = () => Math.floor(Math.random() * ((max - min) + 1)) + min;
-    return type === 'int'
-    ? getInt(min, max)
-    : getArbitrary(min, max);
-  }
-  throw new Error(`
-  Please, enter valid parameters
-  [min = ${min}]
-  [max = ${max}]
-  [type = ${type}]
-  `);
+  const getArbitrary = () => (Math.random() * (max - min)) + min;
+  const getInt = () => Math.floor(Math.random() * ((max - min) + 1)) + min;
+  return type === 'int'
+  ? getInt(min, max)
+  : getArbitrary(min, max);
 };
 
 module.exports = {
